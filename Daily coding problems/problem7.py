8a3148b93c9ee9186ec5d623568a05384939ca08
vedant-jad99 <58693619+vedant-jad99@users.noreply.github.com>
2021-01-09 09:00:00 +0530

    Add files via upload

---
+++ Daily coding problems/problem7.py
@@ -0,0 +1,17 @@
+def possible_combination(string):
+    n = len(string)
+    count = [0] * (n + 1);
+    count[0] = 1;  
+    count[1] = 1;  
+  
+    for i in range(2, n + 1):  
+        count[i] = 0;  
+        if (string[i - 1] > '0'):  
+            count[i] = count[i - 1];  
+  
+        if (string[i - 2] == '1' or (string[i - 2] == '2' and string[i - 1] < '7')):  
+            count[i] += count[i - 2];  
+  
+    return count[n];  
+
+print(possible_combination(input()))
