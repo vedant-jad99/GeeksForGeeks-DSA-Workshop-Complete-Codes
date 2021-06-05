f56eac5c3797d23567dd2e410949cf7a272533c0
vedant-jad99 <58693619+vedant-jad99@users.noreply.github.com>
2021-06-05 12:05:33 +0530

    Update count_neg.py

--- Leetcode/Count Negative Numbers in a Sorted Matrix/count_neg.py
+++ Leetcode/Count Negative Numbers in a Sorted Matrix/count_neg.py
@@ -1,6 +1,5 @@
 '''
-Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative 
-numbers in grid.
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 Example:
         Input - grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
         Output - 8
